1aeb985f3b6cab595e3bb6b3eceb1e6c2ef4b1ec
Kim Price <k_renick@yahoo.com>
2022-09-27 23:03:57 -0500

    completed guage

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,12 @@
 // Test that data is being grabbed
 //<!--./index.js-->
+// Get the endpoint
+const bellyButton = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 initPage();
 
 // ------------- Initialization Function --------------------//
 function initPage() {
-
-  // Get the endpoint
-  const bellyButton = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-
   // Fetch the JSON data and console log it
   d3.json(bellyButton).then(function(data) {
       console.log(data);
@@ -30,7 +28,6 @@ function initPage() {
     createBubbleChart(dropDownId);
     createMetaData(dropDownId);
     createGuage(dropDownId);
-
   });
 }
 
@@ -38,34 +35,64 @@ function initPage() {
 
 //Create Bubble Chart Function
 function createBubbleChart(sample){
-  var trace1 = {
-    x: [],
-    y: [],
-    mode: 'markers',
-    marker: {
-      size: []
-    }
-  };
-  var data = [trace1];
-  var layout = {
-    showlegend: false
-  };
-  Plotly.newPlot('bubble', data, layout);
+  d3.json(bellyButton).then(data => {
+    let samples = data.samples;
+    let selectedArray = samples.filter(s => s.id == sample);
+    let result = selectedArray[0];
+
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
+
+    var trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+
+      }
+    };
+    var data = [trace1];
+    var layout = {
+      showlegend: false,
+      hovermode: 'closest',
+      title: 'Bacteria Cultures per Sample',
+      xaxis: {title: "OTU ID"}
+    };
+    Plotly.newPlot('bubble', data, layout);
+  });
 }
 
 //Create Horizontal Bar Function
 function createBarGraph(sample){
-  var trace1 = {
-    x: [],
-    y: [],
-    type: 'bar',
-    orientation: 'h'
-  };
-  var data = [trace1];
-  var layout = {
-    showlegend: false,
-  };
-  Plotly.newPlot('bar', data, layout);
+  d3.json(bellyButton).then(function(data) {
+    let samples = data.samples;
+    let selectedArray = samples.filter(s => s.id == sample);
+    let result = selectedArray[0];
+
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
+
+    let y_val = otu_ids.slice(0,10).reverse();
+
+    var trace1 = {
+      x: sample_values.slice(0,10).reverse(),
+      y: y_val.map(otu => `OTU ${otu}`).reverse(),
+      type: 'bar',
+      text: otu_labels.slice(0,10).reverse(),
+      orientation: 'h'
+    };
+    var data = [trace1];
+    var layout = {
+      showlegend: false,
+      title: 'Top 10 Bacteria Cultures Found'
+    };
+    Plotly.newPlot('bar', data, layout);
+  });
 }
 
 //Create MetaData Table Function
@@ -76,42 +103,51 @@ function createMetaData(sample){
 
 //Create Guage Function
 function createGuage(sample){
-  let washes = 0;
-  var data = [
-    {
-      type: "indicator",
-      mode: "gauge",
-      title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-      gauge: {
-        axis: { range: [0, 9], tickwidth: 1,},
-        bgcolor: "white",
-        borderwidth: 2,
-        bordercolor: "gray",
-        steps: [
-          { range: [0, 1], color: "#d4d9cc" },
-          { range: [1, 2], color: "#c3ccb4" },
-          { range: [2, 3], color: "#bbc9a3" },
-          { range: [3, 4], color: "#b2c491" },
-          { range: [4, 5], color: "#abc280" },
-          { range: [5, 6], color: "#98b368" },
-          { range: [6, 7], color: "#8eb053" },
-          { range: [7, 8], color: "#79a32f" },
-          { range: [8, 9], color: "#6c9c17" }
-        ],
-        threshold: {
-          line: { color: "red", width: 4 },
-          thickness: 0.75,
-          value: washes
+  d3.json(bellyButton).then(function(data) {
+    let metaData = data.metadata;
+    let selectedData = metaData.filter(m => m.id == sample);
+    let result = selectedData[0];
+
+
+    let washes = result.wfreq;
+    var data = [
+      {
+        type: "indicator",
+        mode: "gauge+number",
+        value: washes,
+        title: { text: "Belly Button Washing Frequency <br> Scrubs per Week"},
+        gauge: {
+          axis: { range: [-.5, 9.5], tickwidth: 1, tick0: 0, dtick: "L1"},
+          bgcolor: "white",
+          bar: { thickness:0},
+          borderwidth: 2,
+          bordercolor: "gray",
+          steps: [
+            { range: [-.5, .5], color: "#d4d9cc" },
+            { range: [.5, 1.5], color: "#c3ccb4" },
+            { range: [1.5, 2.5], color: "#bbc9a3" },
+            { range: [2.5, 3.5], color: "#b2c491" },
+            { range: [3.5, 4.5], color: "#abc280" },
+            { range: [4.5, 5.5], color: "#98b368" },
+            { range: [5.5, 6.5], color: "#8eb053" },
+            { range: [6.5, 7.5], color: "#79a32f" },
+            { range: [7.5, 8.5], color: "#6c9c17" },
+            { range: [8.5, 9.5], color: "#558501"}
+          ],
+          threshold: {
+            line: { color: "black", width: 4 },
+            thickness: 0.75,
+            value: washes + .00001
+          }
         }
       }
-    }
-  ];
-  var layout = {
-    margin: { t: 25, r: 25, l: 25, b: 25 },
-    font: { color: "black", family: "Arial" }
-  };
+    ];
+    var layout = {
+      font: { color: "black", family: "Arial" }
+    };
 
-  Plotly.newPlot('gauge', data, layout);
+    Plotly.newPlot('gauge', data, layout);
+  });
 }
 
 //-------------  On DropDown Change function ---------------//
@@ -123,56 +159,3 @@ function optionChanged(sample){
   createMetaData(sample);
   createGuage(sample);
 }
-
-
-// TODOS:
-// 4.  Create on dropdown change event
-//   1.  get the selected value in dropdown - day3-act5
-//   2.  Gather the data for the selected name
-//   3.  Refresh the graphs and tables with name specific data
-//     a.  MetaData
-          //1. dynamically build table - day3-act4
-//     b.  Bubble
-//     c.  Horizontal Bar (sort ascending, slice top 10)
-//     d.  Guage
-
-//-------Code Snippets from class to use--------------//
-  // Sort the array in ascending order, using an arrow function
-    // let numArray3 = [3, 2, 1];
-    // numArray3.sort((a, b) => a - b);
-  
-  //Slicing
-    // Slice the first two names
-      // let left = names.slice(0, 2);
-
-  //Get json
-    // Get the Roadster endpoint
-      // const roadster = "<URL>";
-
-    // Fetch the JSON data and console log it
-      // d3.json(roadster).then(function(data){
-      //     console.log(data);
-      // });
-
-  //Plotly Charts
-      // let myName = 'Travis Taylor';
-
-      // let title = `${myName}'s First Plotly Chart`;
-      
-      // let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"];
-      
-      // let timesRead = [100, 50, 25];
-      
-      // let trace1 = {
-      //   x: books,
-      //   y: timesRead,
-      //   type: 'bar'
-      // };
-      
-      // let data = [trace1];
-      
-      // let layout = {
-      //   title: title
-      // };
-      
-      // Plotly.newPlot("plot", data, layout);
