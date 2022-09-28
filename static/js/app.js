ade06ec0834d122063005cb4161421960275c800
Kim Price <k_renick@yahoo.com>
2022-09-27 20:28:24 -0500

    dropdown and base plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,154 @@
 // Test that data is being grabbed
 //<!--./index.js-->
 
-fetch('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+// ------------- 1.  Access json data  ------------------------//
+// Get the endpoint
+const bellyButton = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// Fetch the JSON data and console log it
+d3.json(bellyButton).then(function(data) {
+    console.log(data);
+    let names = data.names;
+    let dropDown = d3.select("#selDataset")
 
-//----------- Top 10 OTUs Horizontal Bar Chart ----------------------
-// Reverse the array
-let numArray4 = [1, 2, 3];
-numArray4.reverse()
-console.log(numArray4);
+    //Populate dropdown
+    names.forEach((n) => {
+      let opt = dropDown.append("option")
+      opt.text(n);
+      opt.property("value", n);
+    });
+});
 
-// Sort the array in ascending order, using an arrow function
-let numArray3 = [3, 2, 1];
-numArray3.sort((a, b) => a - b);
-console.log(numArray3);
+//----------------- 2.  Generate Placeholder Graphs and table ---------//
 
+//Bubble
 
-//------------- Bubble Chart displaying each sample ------------------
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 11, 12, 13],
+  mode: 'markers',
+  marker: {
+    size: [40, 60, 80, 100]
+  }
+};
 
+var data = [trace1];
 
+var layout = {
+  showlegend: false
+};
 
+Plotly.newPlot('bubble', data, layout);
 
-//---------------- Display metadata ------------------------------
+//Horizontal Bar
+var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 11, 12, 13],
+  type: 'bar',
+  orientation: 'h'
+};
 
+var data = [trace1];
 
+var layout = {
+  showlegend: false,
+};
 
+Plotly.newPlot('bar', data, layout);
 
-//--------------------- Dropdown functionality -------------------------
+//Guage
+var data = [
+  {
+    type: "indicator",
+    mode: "gauge",
+    title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+    gauge: {
+      axis: { range: [null, 9], tickwidth: 1,},
+      bgcolor: "white",
+      borderwidth: 2,
+      bordercolor: "gray",
+      steps: [
+        { range: [0, 1], color: "#d4d9cc" },
+        { range: [1, 2], color: "#c3ccb4" },
+        { range: [2, 3], color: "#bbc9a3" },
+        { range: [3, 4], color: "#b2c491" },
+        { range: [4, 5], color: "#abc280" },
+        { range: [5, 6], color: "#98b368" },
+        { range: [6, 7], color: "#8eb053" },
+        { range: [7, 8], color: "#79a32f" },
+        { range: [8, 9], color: "#6c9c17" }
+      ],
+      threshold: {
+        line: { color: "red", width: 4 },
+        thickness: 0.75,
+        value: 2.5
+      }
+    }
+  }
+];
+
+var layout = {
+  margin: { t: 25, r: 25, l: 25, b: 25 },
+  font: { color: "black", family: "Arial" }
+};
+
+Plotly.newPlot('gauge', data, layout);
+
+// TODOS:
+// X1.  Access Json data
+// X2.  Populate dropdown with names
+// 3.  Generate placeholder graphs and tables - init function
+//   a.  MetaData
+//   Xb.  Bubble
+//   Xc.  Horizontal Bar
+//   d.  Guage
+// 4.  Create on dropdown change event
+//   1.  get the selected value in dropdown - day3-act5
+//   2.  Gather the data for the selected name
+//   3.  Refresh the graphs and tables with name specific data
+//     a.  MetaData
+          //1. dynamically build table - day3-act4
+//     b.  Bubble
+//     c.  Horizontal Bar (sort ascending, slice top 10)
+//     d.  Guage
+
+//-------Code Snippets from class to use--------------//
+  // Sort the array in ascending order, using an arrow function
+    // let numArray3 = [3, 2, 1];
+    // numArray3.sort((a, b) => a - b);
+  
+  //Slicing
+    // Slice the first two names
+      // let left = names.slice(0, 2);
+
+  //Get json
+    // Get the Roadster endpoint
+      // const roadster = "<URL>";
+
+    // Fetch the JSON data and console log it
+      // d3.json(roadster).then(function(data){
+      //     console.log(data);
+      // });
+
+  //Plotly Charts
+      // let myName = 'Travis Taylor';
+
+      // let title = `${myName}'s First Plotly Chart`;
+      
+      // let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"];
+      
+      // let timesRead = [100, 50, 25];
+      
+      // let trace1 = {
+      //   x: books,
+      //   y: timesRead,
+      //   type: 'bar'
+      // };
+      
+      // let data = [trace1];
+      
+      // let layout = {
+      //   title: title
+      // };
+      
+      // Plotly.newPlot("plot", data, layout);
